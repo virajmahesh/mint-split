0d120de5abe65e5df09031048568df91bf6932e0
Viraj Mahesh <virajmahesh@gmail.com>
2017-01-03 23:45:04 +0400

    Clean up code

--- mint.js
+++ mint.js
@@ -9,7 +9,6 @@ setInterval(function() {
 
   if (splitIcon != null) {
     var actionsDiv = splitIcon.parentNode.parentNode;
-    //console.log(actionsDiv);
 
     // Don't append the 'split in half' button if we have already done it.
     if (actionsDiv.childNodes.length != 7) {
@@ -19,24 +18,17 @@ setInterval(function() {
     var splitHalf = splitIcon.parentNode.cloneNode(true);
     var splitHalfIcon = splitHalf.childNodes[1];
 
-    console.log(splitHalf.childNodes);
-
+    // Set the background color of the new split button.
     splitHalfIcon.style.backgroundImage = 'none';
     splitHalfIcon.style.backgroundColor = '#acd0af';
-
-    //console.log(actionsDiv.childNodes.length);
 
     actionsDiv.appendChild(splitHalf);
 
     splitHalf.onclick = function() {
-
       // Wait 1 second before reading the transaction amount.
       setTimeout(function() {
           var txnAmount = parseFloat(getTransactionAmount());
           var splitAmount = (txnAmount/2).toFixed(2);
-
-          console.log(splitAmount);
-          console.log(txnAmount - splitAmount);
 
           var inputFields = document.getElementsByName('percentAmount');
 
@@ -44,12 +36,8 @@ setInterval(function() {
           inputFields[2].value = (txnAmount - splitAmount).toFixed(2);
 
           var submitButton = document.getElementById('pop-split-submit');
-
-          console.log(submitButton);
-
-          console.log(document.getElementsByName('amount').hasOwnProperty('hidden'));
-
           submitButton.click();
+
         }, 1000);
     };
   }
